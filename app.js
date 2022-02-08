92761d1ec457d4874c25624a4cf5eb920cacf220
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-02-07 18:37:34 -0600

    add new feature file and functions

--- app.js
+++ app.js
@@ -13,25 +13,44 @@ console.log(commandLineArgs);*/
 
 // console.log(sum);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
 //const printProfileData = (profileDataArr) => {
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('================');
+//     console.log('================');
 
-    profileDataArgs.forEach((profileItem) => {
-        console.log(profileItem);
-    });
+//     profileDataArgs.forEach((profileItem) => {
+//         console.log(profileItem);
+//     });
 
-    console.log('================');
-    //clean
-    profileDataArgs.forEach(profileItem =>  console.log(profileItem));
-};
+//     console.log('================');
+//     //clean
+//     profileDataArgs.forEach(profileItem =>  console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//assignment destructuring.
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const[name, github] = profileDataArgs;
+
+const generatePage = require('./src/paage-template');
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
